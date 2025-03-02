1d0133de75f9318e787613b6ece2e2c830762514
Nick Malmquist <nicholasmmalmquist@gmail.com>
2025-03-01 21:21:59 -0500

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile("./dist/index.html");
+  }
+});
